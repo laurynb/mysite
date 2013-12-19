2a7c78a278ba149dc52b0b9cea5c2781c10c7eb7
Lauryn Brown <laurynb@andrew.cmu.edu>
2013-12-19 15:07:37 -0500

    Added dummy models and successfully ran migrations.

---
+++ common/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Question'
+        db.create_table(u'common_question', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('question_text', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal(u'common', ['Question'])
+
+        # Adding model 'Choice'
+        db.create_table(u'common_choice', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('question', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['common.Question'])),
+            ('choice_text', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('votes', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'common', ['Choice'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Question'
+        db.delete_table(u'common_question')
+
+        # Deleting model 'Choice'
+        db.delete_table(u'common_choice')
+
+
+    models = {
+        u'common.choice': {
+            'Meta': {'object_name': 'Choice'},
+            'choice_text': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.Question']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'common.question': {
+            'Meta': {'object_name': 'Question'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'question_text': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['common']
